3ab973ecb4c6420422ac3e10b4605c34972035c2
Dharani 007 <dharanimeenatchisundharam112@gmail.com>
2023-01-27 19:50:21 +0530

    Add : Solving using Switch case Statement

---
+++ UC4/EmployeeWageComputationProblem-UC4-CaseStatement/empWageComputation.sh
@@ -0,0 +1,20 @@
+echo "Welcome to Employee Wage Computation Program"
+
+isPartTime=1
+isFullTime=2
+empRatePerHrs=20
+
+randomCheck=$((RANDOM%3))
+
+case $randomCheck in $isFullTime)
+			empHrs=8;;
+		     $isPartTime)
+			empHrs=4;;
+		     *)
+		     $empHrs=0;;
+esac
+
+salary=$(($empHrs*$empRatePerHrs))
+echo "salary=$salary"
+
+
