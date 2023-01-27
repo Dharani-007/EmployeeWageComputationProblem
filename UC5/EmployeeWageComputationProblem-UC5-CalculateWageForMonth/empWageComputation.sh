c7a14290633d98341602a950b21b8253343ea587
Dharani 007 <dharanimeenatchisundharam112@gmail.com>
2023-01-27 21:52:03 +0530

    Add : Solving using Switch case Statement

---
+++ UC5/EmployeeWageComputationProblem-UC5-CalculateWageForMonth/empWageComputation.sh
@@ -0,0 +1,28 @@
+echo "Welcome to Employee Wage Computation Program"
+
+isPartTime=1
+isFullTime=2
+empRatePerHrs=20
+totalSalary=0
+numWorkingDays=20
+
+for (( day=1;day<=$numWorkingDays; day++ ))
+do
+	empCheck=$((RANDOM%3))
+	case $empCheck in $isFullTime)
+			 empHrs=8;;
+		         $isPartTime)
+			 empHrs=4;;
+		        *)
+		        empHrs=0;;
+esac
+
+salary=$(($empHrs*$empRatePerHrs))
+totalSalary=$(($totalSalary+$salary))
+
+done
+
+echo "Employee has earned $totalSalary in a month"
+
+
+
